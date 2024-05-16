a48284e3e4ae34a311c38236d1eaa6f8c73bdacf
최승윤 <81810669+shakingcarrot@users.noreply.github.com>
2024-05-16 19:40:57 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240516/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Character> stack = new Stack<>();
+        String str = br.readLine();
+        int len = str.length();
+        for(int i = 0; i < len; i++) {
+            char c = str.charAt(i);
+            if(c == '(') {
+                stack.push(c);
+            }
+            else if(c == ')') {
+                if(stack.isEmpty()) {
+                    System.out.println("No");
+                }else{
+                    stack.pop();
+                }
+
+            }
+        }
+        if(!stack.isEmpty()) {
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+    }
+}
