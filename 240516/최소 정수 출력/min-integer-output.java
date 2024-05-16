6f6975a2938f6f464747ba566d1fd330efcab632
최승윤 <81810669+shakingcarrot@users.noreply.github.com>
2024-05-16 20:33:02 +0900

    Title: 최소 정수 출력, Time: 966ms, Memory: 24MB, Status: Passed - CodeTree

--- 240516/최소 정수 출력/min-integer-output.java
+++ 240516/최소 정수 출력/min-integer-output.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int N = Integer.parseInt(br.readLine());
         int x;
